2680a73c3a519f33e18cd789a1f45268c084eecd
joverzhang <joverzh@gmail.com>
2021-10-17 17:41:20 +0800

    feat: 新增全局 Message 组件

--- src/main.js
+++ src/main.js
@@ -3,22 +3,24 @@ import App from './App.vue'
 import router from './Routes'
 import store from './store/index'
 import vuetify from './plugins/vuetify'
-import * as VueGoogleMaps from 'vue2-google-maps';
-import Toast from "vue-toastification";
-import "vue-toastification/dist/index.css";
+import * as VueGoogleMaps from 'vue2-google-maps'
+import Toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
+import Message from '@/utils/Message'
 
-Vue.use(Toast);
+Vue.use(Toast)
 
 Vue.use(VueGoogleMaps, {
   load: {
     key: 'AIzaSyB7OXmzfQYua_1LEhRdqsoYzyJOPh9hGLg',
   },
-});
+})
 
 Vue.config.productionTip = false
+Vue.prototype.$message = Message
 
 new Vue({
   vuetify,
   router,
-  render: h => h(App), store
+  render: h => h(App), store,
 }).$mount('#app')
